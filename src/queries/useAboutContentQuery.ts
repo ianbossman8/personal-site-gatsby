9d5a2ce667794f46e8de19d45e1c9bd2aad8d8b9
ian chan <ian035@hotmail.com>
2021-02-14 00:42:06 +0000

    fix:remove post about

--- src/queries/useAboutContentQuery.ts
+++ src/queries/useAboutContentQuery.ts
@@ -7,15 +7,14 @@ type AboutQueryResponse = {
 }
 
 export function useAboutContentQuery() {
-  const { markdownRemark } = useStaticQuery<AboutQueryResponse>(
-    graphql`
-      query {
-        markdownRemark(fields: { slug: { eq: "/about/me/" } }) {
-          html
-        }
-      }
-    `
-  )
-
-  return markdownRemark.html
+  // const { markdownRemark } = useStaticQuery<AboutQueryResponse>(
+  //   graphql`
+  //     query {
+  //       markdownRemark(fields: { slug: { eq: "/siteInfo/about-me/" } }) {
+  //         html
+  //       }
+  //     }
+  //   `
+  // )
+  // return markdownRemark.html
 }
