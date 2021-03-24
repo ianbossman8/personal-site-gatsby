51838e0cc86f855103afe06fb2ba4b2b8c85389c
ian chan <ian035@hotmail.com>
2021-03-24 00:56:41 +0000

    fix:stylings h1, figure margin, up button

--- src/templates/styles/blogs.styles.ts
+++ src/templates/styles/blogs.styles.ts
@@ -1,18 +1,16 @@
 import styled from 'styled-components'
 import { CommonDumpStyles } from './dump.styles'
 
-export const BlogPage = styled.div`
-  ${CommonDumpStyles};
-`
-
-export const BlogPostContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem 0;
-`
-
 export const ImgHolder = styled.figure`
   width: 100%;
   max-width: 60rem;
+  margin: 0;
+`
+
+export const BlogPage = styled.div`
+  ${CommonDumpStyles};
+
+  ${ImgHolder} {
+    margin: 1rem 0;
+  }
 `
